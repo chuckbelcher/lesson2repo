52a296b746401dd58f82cfe420edb345f7e527c6
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-09 16:17:37 -0400

    Initial Commit

---
+++ Lesson16Challenge/Lesson16ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson16ChallengeApp.swift
+//  Lesson16Challenge
+//
+//  Created by Chuck Belcher on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Lesson16ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
